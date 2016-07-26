24a0a3e59c9a55dc074987d191ba9a4d716f9043
Iuliia Petrova <iuliia_petrova@epam.com>
2016-07-26 14:45:36 +0300

    SeleniumWebdriverBasics,hometask#1

---
+++ MailTest/MailTest/UnitTest2.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace MailTest
+{
+  
+    public class UnitTest2
+    {
+        
+        public void TestMethod1()
+        {
+        }
+    }
+}
